b3e22b8b13346004709e148e83b1114b5e619bb4
doluwasegun <doluwasegun@seamfix.com>
2017-08-21 12:21:44 +0100

    Master init

---
+++ src/main/java/ng/autotopup/engagement_utils/Utils.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ng.autotopup.engagement_utils;
+
+import javax.ejb.Stateless;
+
+@Stateless(name = "EngUtils")
+public class Utils {
+
+}
